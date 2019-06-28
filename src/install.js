9f17ca863e19fd0bfc767eb5a03bebad3a5c0e0f
eva <eva.de.haas@liones.nl>
2019-06-28 10:46:30 +0200

    Upgrade to SDK 7.7

--- src/install.js
+++ src/install.js
@@ -1,101 +1,96 @@
-define([
-	'fontoxml-base-flow/addCustomMutation',
-	'fontoxml-blueprints/readOnlyBlueprint',
-	'fontoxml-documents/documentsManager',
-	'fontoxml-dom-identification/getNodeId',
-	'fontoxml-operations/addTransform',
-	'fontoxml-selection/selectionManager',
-	'fontoxml-selectors/evaluateXPathToFirstNode',
-	'fontoxml-selectors/evaluateXPathToStrings',
+import addCustomMutation from 'fontoxml-base-flow/src/addCustomMutation.js';
+import readOnlyBlueprint from 'fontoxml-blueprints/src/readOnlyBlueprint.js';
+import documentsManager from 'fontoxml-documents/src/documentsManager.js';
+import getNodeId from 'fontoxml-dom-identification/src/getNodeId.js';
+import addTransform from 'fontoxml-operations/src/addTransform.js';
+import selectionManager from 'fontoxml-selection/src/selectionManager.js';
+import evaluateXPathToFirstNode from 'fontoxml-selectors/src/evaluateXPathToFirstNode.js';
+import evaluateXPathToStrings from 'fontoxml-selectors/src/evaluateXPathToStrings.js';
+import insertNodeAndRemoveFromSiblings from './api/insertNodeAndRemoveFromSiblingsCustomMutation.js';
+import replaceNodesWithMappedStructure from './api/replaceNodesWithMappedStructureCustomMutation.js';
 
-	'./api/insertNodeAndRemoveFromSiblingsCustomMutation',
-	'./api/replaceNodesWithMappedStructureCustomMutation'
-], function (
-	addCustomMutation,
-	readOnlyBlueprint,
-	documentsManager,
-	getNodeId,
-	addTransform,
-	selectionManager,
-	evaluateXPathToFirstNode,
-	evaluateXPathToStrings,
+export default function install() {
+	addCustomMutation('insert-node-and-remove-from-siblings', insertNodeAndRemoveFromSiblings);
+	addCustomMutation('replace-nodes-with-mapped-structure', replaceNodesWithMappedStructure);
 
-	insertNodeAndRemoveFromSiblings,
-	replaceNodesWithMappedStructure
-	) {
-	'use strict';
+	addTransform('setSequenceValue', function setSequenceValue(stepData) {
+		var contextNode = documentsManager.getNodeById(stepData.contextNodeId);
+		if (!contextNode || !stepData.sequenceValueXPathQuery) {
+			if (!stepData.operationState) {
+				stepData.operationState = {};
+			}
+			stepData.operationState.enabled = false;
+			return stepData;
+		}
 
-	return function install () {
-		addCustomMutation('insert-node-and-remove-from-siblings', insertNodeAndRemoveFromSiblings);
-		addCustomMutation('replace-nodes-with-mapped-structure', replaceNodesWithMappedStructure);
+		var sequenceValues = evaluateXPathToStrings(
+			stepData.sequenceValueXPathQuery,
+			contextNode,
+			readOnlyBlueprint
+		);
+		stepData.value = String(sequenceValues.length + 1);
 
-		addTransform(
-			'setSequenceValue',
-			function setSequenceValue (stepData) {
-				var contextNode = documentsManager.getNodeById(stepData.contextNodeId);
-				if (!contextNode || !stepData.sequenceValueXPathQuery) {
-					if (!stepData.operationState) {
-						stepData.operationState = {};
-					}
-					stepData.operationState.enabled = false;
-					return stepData;
+		if (sequenceValues.length > 0) {
+			for (var i = 1; i <= sequenceValues.length; i++) {
+				if (sequenceValues.indexOf(String(i)) === -1) {
+					stepData.value = String(i);
+					break;
 				}
+			}
+		}
 
-				var sequenceValues = evaluateXPathToStrings(stepData.sequenceValueXPathQuery, contextNode, readOnlyBlueprint);
-				stepData.value = String(sequenceValues.length + 1);
+		return stepData;
+	});
 
-				if (sequenceValues.length > 0) {
-					for (var i = 1; i <= sequenceValues.length; i++) {
-						if (sequenceValues.indexOf(String(i)) === -1) {
-							stepData.value = String(i);
-							break;
-						}
-					}
-				}
+	addTransform(
+		'disableOperationIfContextNode',
+		function(stepData) {
+			return stepData;
+		},
+		function disableOperationIfContextNode(stepData) {
+			var contextNode = documentsManager.getNodeById(stepData.contextNodeId);
+			if (contextNode) {
+				stepData.operationState = {
+					enabled: false
+				};
+			}
+			return stepData;
+		}
+	);
 
+	addTransform(
+		'setContextNodeIdToPrecedinglcInteraction',
+		function setContextNodeIdToPrecedinglcInteraction(stepData) {
+			var selectedElement = selectionManager.getSelectedElement();
+			if (!selectedElement) {
 				return stepData;
 			}
-		);
 
-		addTransform(
-			'disableOperationIfContextNode',
-			function (stepData) {
-				return stepData;
-			},
-			function disableOperationIfContextNode (stepData) {
-				var contextNode = documentsManager.getNodeById(stepData.contextNodeId);
-				if (contextNode) {
-					stepData.operationState = {
-						enabled: false
-					};
-				}
-				return stepData;
+			var lcInteractionNode;
+			var selectionAncestor = evaluateXPathToFirstNode(
+				'ancestor-or-self::*[self::section or self::lcSummary]',
+				selectedElement,
+				readOnlyBlueprint
+			);
+			if (selectionAncestor) {
+				lcInteractionNode = evaluateXPathToFirstNode(
+					'preceding-sibling::lcInteraction[1]',
+					selectionAncestor,
+					readOnlyBlueprint
+				);
+			} else {
+				lcInteractionNode = evaluateXPathToFirstNode(
+					'self::learningAssessmentbody/child::lcInteraction[last()]',
+					selectedElement,
+					readOnlyBlueprint
+				);
 			}
-		);
 
-		addTransform(
-			'setContextNodeIdToPrecedinglcInteraction',
-			function setContextNodeIdToPrecedinglcInteraction (stepData) {
-				var selectedElement = selectionManager.getSelectedElement();
-				if (!selectedElement) {
-					return stepData;
-				}
-
-				var lcInteractionNode;
-				var selectionAncestor = evaluateXPathToFirstNode('ancestor-or-self::*[self::section or self::lcSummary]', selectedElement, readOnlyBlueprint);
-				if (selectionAncestor) {
-					lcInteractionNode = evaluateXPathToFirstNode('preceding-sibling::lcInteraction[1]', selectionAncestor, readOnlyBlueprint);
-				}
-				else {
-					lcInteractionNode = evaluateXPathToFirstNode('self::learningAssessmentbody/child::lcInteraction[last()]', selectedElement, readOnlyBlueprint);
-				}
-
-				if (lcInteractionNode) {
-					stepData.contextNodeId = getNodeId(lcInteractionNode);
-				}
-
-				return stepData;
+			if (lcInteractionNode) {
+				stepData.contextNodeId = getNodeId(lcInteractionNode);
 			}
-		);
-	};
-});
+
+			return stepData;
+		}
+	);
+}
